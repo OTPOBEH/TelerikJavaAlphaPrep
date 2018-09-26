3e64b3aaea71f4df6155f20fb9673ba19c3b9f51
OTPOBEH <3akobah@gmail.com>
2018-09-26 22:35:22 +0300

    Description to Kolbi added...

--- src/Kolbi.java
+++ src/Kolbi.java
@@ -26,6 +26,7 @@ public class Kolbi {
     public static void main(String[] args) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         String[] inputValues = in.readLine().split(" ");
+        System.out.println();
         double liquid = Double.parseDouble(inputValues[1]);
         int needContainerNumber = (int) Math.ceil((1 + Math.sqrt(1 + 8 * liquid)) / 2) / 2 * 2 + 1;
         System.out.println(needContainerNumber);
