0f60020414c50537da693db826167d95fba9beaa
OTPOBEH <3akobah@gmail.com>
2018-09-26 22:13:30 +0300

    initial commit

--- src/Kolbi.java
+++ src/Kolbi.java
@@ -7,8 +7,7 @@ public class Kolbi {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         String[] inputValues = in.readLine().split(" ");
         double liquid = Double.parseDouble(inputValues[1]);
-        double result1 = (1 + Math.sqrt(1 + 8 * liquid)) / 2;
-        int needContainerNumber = (int) Math.ceil(result1) / 2 * 2 + 1;
+        int needContainerNumber = (int) Math.ceil((1 + Math.sqrt(1 + 8 * liquid)) / 2) / 2 * 2 + 1;
         System.out.println(needContainerNumber);
     }
 }
