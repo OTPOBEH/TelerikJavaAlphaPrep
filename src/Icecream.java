e2f74032d2ce93a643637a5d19520605f7042e3f
OTPOBEH <3akobah@gmail.com>
2018-09-27 07:34:10 +0300

    Description to Kolbi added...

--- src/Icecream.java
+++ src/Icecream.java
@@ -7,10 +7,12 @@ public class Icecream {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String[] input = reader.readLine().split(" ");
 
-        int elephantsNumber = Integer.parseInt(input[0]);
-        String ballsByElephant = input[1].replace("0","");
+        int elephantsLeft = Integer.parseInt(input[0]);
 
-        if (elephantsNumber <= ballsByElephant.length()) System.out.println(0);
-        else System.out.println(elephantsNumber - input[1].length());
+        for (int i = 0; elephantsLeft != 0 && i < input[1].length(); i++) {
+            if (input[1].charAt(i) != '0') elephantsLeft--;
+        }
+
+        System.out.println(elephantsLeft);
     }
 }
