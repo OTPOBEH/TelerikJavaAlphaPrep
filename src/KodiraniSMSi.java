7e79d37da25b35cefbd8a13316974e94d890f925
OTPOBEH <3akobah@gmail.com>
2018-09-30 22:34:01 +0300

    Mutacia solved

---
+++ src/KodiraniSMSi.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class KodiraniSMSi {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String input = reader.readLine();
+        char currentChar = input.charAt(0);
+
+        StringBuilder printout = new StringBuilder().append(currentChar);
+
+        for (int i = 1; i < input.length(); i++) {
+            currentChar = input.charAt(i);
+            StringBuilder toAdd = new StringBuilder(printout);
+            printout.append(currentChar);
+            printout.append(toAdd);
+        }
+        System.out.println(printout);
+    }
+}
