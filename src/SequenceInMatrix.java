c75ea1be49fca9f30e664d2046fc53260dca8053
OTPOBEH <3akobah@gmail.com>
2018-10-04 12:40:57 +0300

    SequenceInMatrix solved

--- src/SequenceInMatrix.java
+++ src/SequenceInMatrix.java
@@ -5,47 +5,145 @@ import java.util.Arrays;
 public class SequenceInMatrix {
     public static void main(String[] args) throws Exception {
 
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        int[] arraySize = Arrays.stream(in.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        String[][] matrix = new String[arraySize[0]][];
+        String[] sizeInput = reader.readLine().split(" ");
+
+        final int rows = Integer.parseInt(sizeInput[0]);
+
+        String[][] matrix = new String[rows][];
 
         for (int i = 0; i < matrix.length; i++) {
-            matrix[i] = in.readLine().split(" ");
+            matrix[i] = reader.readLine().split(" ");
         }
 
-        int maxCounter = 1;
+        int maxSequence = 0;
 
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                int currentSequence = checkHor(matrix, i, j) + checkVert(matrix, i, j) + checkDiagOne(matrix, i, j) + checkDiagTwo(matrix, i, j) + 1;
+                if (currentSequence > maxSequence) maxSequence = currentSequence;
+            }
+        }
+
+        System.out.println(maxSequence);
 
     }
 
-    private int CheckLeftDiagonal(String[] matrix, int[] current) {
-
+    private static int checkHor(String[][] matrix, int row, int col) {
         int counter = 0;
 
+        //Check right
+        for (int i = col; i < matrix[row].length - 1; i++) {
+            if (matrix[row][i].equals(matrix[row][i + 1])) {
+                counter++;
+            } else {
+                break;
+            }
+        }
+
+        //Check left
+        for (int i = col; i >= 1; i--) {
+            if (matrix[row][i].equals(matrix[row][i - 1])) {
+                counter++;
+            } else {
+                break;
+            }
+        }
 
         return counter;
     }
-    private int CheckRightDiagonal(String[] matrix, int[] current) {
 
+    private static int checkVert(String[][] matrix, int row, int col) {
         int counter = 0;
 
+        //Check up
+        for (int i = row; i >= 1; i--) {
+            if (matrix[i][col].equals(matrix[i - 1][col])) {
+                counter++;
+            } else {
+                break;
+            }
+        }
+
+        //Check down
+        for (int i = row; i < matrix.length - 1; i++) {
+            if (matrix[i][col].equals(matrix[i + 1][col])) {
+                counter++;
+            } else {
+                break;
+            }
+        }
 
         return counter;
     }
-    private int CheckHorizontal(String[] matrix, int[] current) {
 
+    private static int checkDiagOne(String[][] matrix, int row, int col) {
         int counter = 0;
 
+        //Check left-up
+        int i = row;
+        int j = col;
+        while (i > 0 && j > 0) {
+            if (matrix[i][j].equals(matrix[i - 1][j - 1])) {
+                counter++;
+                i--;
+                j--;
+
+            } else {
+                break;
+            }
+        }
+
+        //Check right-down
+        i = row;
+        j = col;
+
+        while (i < matrix.length - 1 && j < matrix[0].length - 1) {
+            if (matrix[i][j].equals(matrix[i + 1][j + 1])) {
+                counter++;
+                i++;
+                j++;
+
+            } else {
+                break;
+            }
+        }
 
         return counter;
     }
-    private int CheckVertical(String[] matrix, int[] current) {
 
+    private static int checkDiagTwo(String[][] matrix, int row, int col) {
         int counter = 0;
+        int i = row;
+        int j = col;
 
+        //Check right-up
+        while (i > 0 && j < matrix[0].length - 1) {
+            if (matrix[i][j].equals(matrix[i - 1][j + 1])) {
+                counter++;
+                i--;
+                j++;
+
+            } else {
+                break;
+            }
+        }
+
+        //Check left-down
+        i = row;
+        j = col;
+        while (i < matrix.length - 1 && j > 0) {
+            if (matrix[i][j].equals(matrix[i + 1][j - 1])) {
+                counter++;
+                i++;
+                j--;
+
+            } else {
+                break;
+            }
+        }
 
         return counter;
     }
-
 }
