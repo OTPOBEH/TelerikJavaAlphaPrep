6d6f834cdf1343b16381916e4384f57fc51c9cfe
OTPOBEH <3akobah@gmail.com>
2018-10-09 13:43:01 +0300

    Not solved

--- src/Circle.java
+++ src/Circle.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Circle {
     public static void main(String[] args) throws Exception {
@@ -13,19 +14,28 @@ public class Circle {
         int currentIndex = Integer.parseInt(teacherInput[1]) - 1;
 
         int[] numbers = new int[participantsCount];
+        Integer[] participantTracking = new Integer[participantsCount];
 
-        ArrayList<Integer> circle = new ArrayList<>();
         for (int i = 0; i < participantsCount; i++) {
             numbers[i] = Integer.parseInt(reader.readLine());
-            circle.add(i);
+            participantTracking[i] = 1;
         }
 
-        while (circle.size() != 1) {
-            int steps = numbers[circle.get(currentIndex)];
-            circle.remove(currentIndex);
-            currentIndex = (currentIndex + steps - 1) % circle.size();
+        while (participantsCount > 1) {
+            int steps = numbers[currentIndex];
+            participantTracking[currentIndex] = 0;
+
+            int currentSteps = 0;
+
+            currentIndex = (currentIndex + steps) % numbers.length;
+
+            while (participantTracking[currentIndex] == 0) {
+                currentSteps++;
+                currentIndex = (currentIndex + currentSteps) % numbers.length;
+            }
+            participantsCount--;
         }
 
-        System.out.println(circle.get(0) + 1);
+        System.out.println(Arrays.asList(participantTracking).indexOf(1) + 1);
     }
 }
