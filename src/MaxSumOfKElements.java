03c8eeb2159277fe161766ab5c753fcf21a6e9da
OTPOBEH <3akobah@gmail.com>
2018-10-09 13:43:45 +0300

    Updated

--- src/MaxSumOfKElements.java
+++ src/MaxSumOfKElements.java
@@ -34,20 +34,22 @@ Input 	Output
 7
  */
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class MaxSumOfKElements {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = in.nextInt();
-        int k = in.nextInt();
+        int n = Integer.parseInt(reader.readLine());
+        int k = Integer.parseInt(reader.readLine());
 
-        Integer [] numbers = new Integer[n];
+        Integer[] numbers = new Integer[n];
 
         for (int i = 0; i < numbers.length; i++) {
-            numbers[i] = in.nextInt();
+            numbers[i] = Integer.parseInt(reader.readLine());
         }
 
         Arrays.sort(numbers);
