3515872dcdf97c29586666d68b31705e498f87c5
OTPOBEH <3akobah@gmail.com>
2018-10-09 13:43:23 +0300

    updated

--- src/FermatPrimalityTest.java
+++ src/FermatPrimalityTest.java
@@ -1,92 +1,55 @@
 /**
-
  ** Java Program to Implement Fermat Primality Test Algorithm
-
+ * boolean prime = fp.isPrime(num, k); конструктор
  **/
 import java.util.Scanner;
-
 import java.util.Random;
 
-/** Class FermatPrimality **/
-
 public class FermatPrimalityTest
-
 {
-
     /** Function to check if prime or not **/
-
     public boolean isPrime(long n, int iteration)
-
     {
+        /** base case 1 **/
+        if (n == 0) return false;
 
-        /** base case **/
-
-        if (n == 0 || n == 1)
-
-            return false;
-
-        /** base case - 2 is prime **/
-
-        if (n == 2)
-
-            return true;
+        /** base case 2 **/
+        if ( n > 0 && n < 4) return true;
 
         /** an even number other than 2 is composite **/
-
-        if (n % 2 == 0)
-
-            return false;
-
-
+        if (n % 2 == 0) return false;
 
         Random rand = new Random();
 
         for (int i = 0; i < iteration; i++)
 
         {
-
             long r = Math.abs(rand.nextLong());
-
             long a = r % (n - 1) + 1;
-
-            if (modPow(a, n - 1, n) != 1)
-
-                return false;
-
+            if (modPow(a, n - 1, n) != 1) return false;
         }
 
         return true;
-
     }
 
     /** Function to calculate (a ^ b) % c **/
-
     public long modPow(long a, long b, long c)
-
     {
-
         long res = 1;
 
         for (int i = 0; i < b; i++)
-
         {
-
             res *= a;
-
             res %= c;
-
         }
 
         return res % c;
-
     }
 
     /** Main function **/
-
     public static void main (String[] args)
 
     {
-
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Fermat Primality Algorithm Test\n");
