5ad490ce0b7eaeb59a89e88254626dca66c1205e
OTPOBEH <3akobah@gmail.com>
2018-10-10 14:20:06 +0300

    Not Solved

--- src/CokisProducts.java
+++ src/CokisProducts.java
@@ -1,11 +1,15 @@
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.HashMap;
+import java.util.InputMismatchException;
 
 public class CokisProducts {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        InputReader customReader = new InputReader();
 
         HashMap<String, Double> prices = new HashMap<>();
 
@@ -25,51 +29,92 @@ public class CokisProducts {
 
             double price = Double.parseDouble(temp.reverse().toString());
 
-            temp = new StringBuilder();
-            j--;
-            for (; j >= 0; j--) {
-                temp.append(productPriceInput.charAt(j));
-            }
-
-            String itemName = temp.reverse().toString();
+            String itemName = productPriceInput.substring(0, j);
 
             prices.put(itemName, price);
-
         }
 
         int invoices = Integer.parseInt(reader.readLine());
 
-
         for (int i = 0; i < invoices; i++) {
             double total = 0;
-            String[] invoiceInput = reader.readLine().split(", ");
 
-            for (int j = 0; j < invoiceInput.length; j++) {
-                if (prices.containsKey(invoiceInput[j])) total += prices.get(invoiceInput[j]);
-                else {
-                    StringBuilder temp = new StringBuilder();
+            String currentProduct = customReader.readString();
 
-                    int charAt = 0;
-                    char current = invoiceInput[j].charAt(charAt);
-                    while (Character.isDigit(current)) {
-                        temp.append(current);
-                        charAt++;
-                        current = invoiceInput[j].charAt(charAt);
-                    }
+            if (prices.containsKey(currentProduct)) total += prices.get(currentProduct);
+            else {
+                StringBuilder temp = new StringBuilder();
 
-                    int quantity = Integer.parseInt(temp.toString());
-
+                int charAt = 0;
+                char current = currentProduct.charAt(charAt);
+                while (Character.isDigit(current)) {
+                    temp.append(current);
                     charAt++;
-                    String itemName = invoiceInput[j].substring(charAt);
-                    double currentPrice = (prices.get(itemName)) * quantity;
-                    total += currentPrice;
+                    current = currentProduct.charAt(charAt);
                 }
+
+                int quantity = Integer.parseInt(temp.toString());
+
+                charAt++;
+                String itemName = currentProduct.substring(charAt);
+                double currentPrice = (prices.get(itemName)) * quantity;
+                total += currentPrice;
+
             }
+
             System.out.printf("%.2f\n", total);
         }
     }
-}
 
+    public static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+
+        InputReader() {
+            this.stream = System.in;
+        }
+
+        private int read() {
+            if (numChars == -1)
+                throw new InputMismatchException();
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0)
+                    return -1;
+            }
+            return buf[curChar++];
+        }
+
+        public String readString() {
+            final StringBuilder stringBuilder = new StringBuilder();
+            int c = read();
+
+            boolean skippedFirstSpaceChar = false;
+            while (isSpaceChar(c))
+                c = read();
+            do {
+                if (c == ' ' && !skippedFirstSpaceChar) {
+                    skippedFirstSpaceChar = true;
+                    continue;
+                }
+                stringBuilder.append((char) c);
+                c = read();
+            } while (!isSpaceChar(c));
+            return stringBuilder.toString();
+        }
+
+        boolean isSpaceChar(int c) {
+            return c == ',' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+    }
+}
 
 
 /*
