a042eb153ba5088607a4fdd5dd81332702b5da49
OTPOBEH <3akobah@gmail.com>
2018-10-14 20:06:03 +0300

    3DSlices Solved

--- src/ThreeDSlices.java
+++ src/ThreeDSlices.java
@@ -1,111 +1,145 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.io.InputStream;
+import java.util.InputMismatchException;
 
 public class ThreeDSlices {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
+        InputReader inputReader = new InputReader();
 
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-
-        int[] cuboidSize = Arrays.stream(in.readLine().split(" "))
-                .mapToInt(Integer::parseInt)
-                .toArray();
-        int w = cuboidSize[0];
-        int h = cuboidSize[1];
-        int d = cuboidSize[2];
-
-        long[][] cuboid = new long[h][];
-
-        for (int row = 0; row < h; row++) {
-            cuboid[row] = Arrays.stream(in.readLine().split("(\\s\\|\\s)|(\\s)"))
-                    .mapToLong(Integer::parseInt)
-                    .toArray();
+        int[] cuboidSize = new int[3];
+        for (int i = 0; i < cuboidSize.length; i++) {
+            cuboidSize[i] = inputReader.readInt();
         }
 
-        int count = CountEqualSums(cuboid, w, h, d);
-        System.out.println(count);
-    }
+        int width = cuboidSize[0];
+        int height = cuboidSize[1];
+        int depth = cuboidSize[2];
 
-    public static int CountEqualSums(long[][] cuboid, int width, int height, int depth) {
+        int[] sumsByWidth = new int[width];
+        int[] sumsByHeght = new int[height];
+        int[] sumsByDepth = new int[depth];
+
+        int[][][] cuboid = new int[height][depth][width];
+
+        for (int h = 0; h < height; h++) {
+            for (int d = 0; d < depth; d++) {
+                for (int w = 0; w < width; w++) {
+                    cuboid[h][d][w] = inputReader.readInt();
+                    sumsByWidth[w] += cuboid[h][d][w];
+                    sumsByHeght[h] += cuboid[h][d][w];
+                    sumsByDepth[d] += cuboid[h][d][w];
+                }
+            }
+        }
+
         int counter = 0;
 
-        long[] sumsByD = new long[depth];
-        long[] sumsByH = new long[height];
+//Checking slices by depth
+        for (int slice = 1; slice < depth; slice++) {
 
-        int sumsByWAdded = 0;
-        int sumsByHAdded = 0;
+            int sum1 = 0;
+            int sum2 = 0;
 
-        for (int j = 1; j < width; j++) {
-            int w = 0;
-            long sum1 = 0;
-            long sum2 = 0;
-            int limit = width;
+            for (int index = 0; index < slice; index++) {
+                sum1 += sumsByDepth[index];
+            }
 
-            int currentSlice = j;
-
-            for (int d = 0; d < depth; d++) {
-
-                for (; w < currentSlice; w++) {
-                    for (int h = 0; h < height; h++) {
-                        sum1 += cuboid[h][w];
-                        if (sumsByHAdded < depth) sumsByH[h] += cuboid[h][w];
-                    }
-                }
-
-                for (w = currentSlice; w < limit; w++) {
-                    for (int h = 0; h < height; h++) {
-                        sum2 += cuboid[h][w];
-                        if (sumsByHAdded < depth) sumsByH[h] += cuboid[h][w];
-                    }
-                }
-
-                limit += width;
-                currentSlice = w + j;
-                sumsByHAdded++;
-
-                if (sumsByWAdded != depth) {
-                    sumsByD[d] = sum1 + sum2;
-                    sumsByWAdded++;
-                }
+            for (int index = slice; index < depth; index++) {
+                sum2 += sumsByDepth[index];
             }
 
             if (sum1 == sum2) counter++;
         }
 
-        for (int i = 1; i < depth; i++) {
-            int firstSlice = i;
-            long sum1 = 0;
-            long sum2 = 0;
 
-            for (int j = 0; j < firstSlice; j++) {
+//Checking slices by width
+        for (int slice = 1; slice < width; slice++) {
 
-                sum1 += sumsByD[j];
+            int sum1 = 0;
+            int sum2 = 0;
+
+            for (int index = 0; index < slice; index++) {
+                sum1 += sumsByWidth[index];
             }
 
-            for (int j = firstSlice; j < depth; j++) {
-                sum2 += sumsByD[j];
-            }
-
-            if (sum1 == sum2) counter++;
-        }
-
-        for (int i = 1; i < height; i++) {
-            int firstSlice = i;
-            long sum1 = 0;
-            long sum2 = 0;
-
-            for (int j = 0; j < firstSlice; j++) {
-                sum1 += sumsByH[j];
-            }
-
-            for (int j = firstSlice; j < height; j++) {
-                sum2 += sumsByH[j];
+            for (int index = slice; index < width; index++) {
+                sum2 += sumsByWidth[index];
             }
 
             if (sum1 == sum2) counter++;
         }
 
-        return counter;
+//Checking slices by height
+        for (int slice = 1; slice < height; slice++) {
+
+            int sum1 = 0;
+            int sum2 = 0;
+
+            for (int index = 0; index < slice; index++) {
+                sum1 += sumsByHeght[index];
+            }
+
+            for (int index = slice; index < height; index++) {
+                sum2 += sumsByHeght[index];
+            }
+
+            if (sum1 == sum2) counter++;
+        }
+
+
+        System.out.println(counter);
+    }
+
+    public static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+
+        InputReader() {
+            this.stream = System.in;
+        }
+
+        private int read() {
+            if (numChars == -1)
+                throw new InputMismatchException();
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0)
+                    return -1;
+            }
+            return buf[curChar++];
+        }
+
+        int readInt() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9') {
+                    throw new InputMismatchException();
+                }
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        boolean isSpaceChar(int c) {
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1 || c == '|';
+        }
     }
 }
