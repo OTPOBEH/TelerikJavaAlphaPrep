58486664dac6dce153bc63e34673ef9d91af5d64
OTPOBEH <3akobah@gmail.com>
2018-10-19 12:38:42 +0300

    Some updates

--- src/Circle.java
+++ src/Circle.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.LinkedList;
+
 
 public class Circle {
     public static void main(String[] args) throws Exception {
@@ -14,28 +14,19 @@ public class Circle {
         int currentIndex = Integer.parseInt(teacherInput[1]) - 1;
 
         int[] numbers = new int[participantsCount];
-        Integer[] participantTracking = new Integer[participantsCount];
 
+        LinkedList<Integer> circle = new LinkedList<>();
         for (int i = 0; i < participantsCount; i++) {
             numbers[i] = Integer.parseInt(reader.readLine());
-            participantTracking[i] = 1;
+            circle.add(i);
         }
 
-        while (participantsCount > 1) {
-            int steps = numbers[currentIndex];
-            participantTracking[currentIndex] = 0;
-
-            int currentSteps = 0;
-
-            currentIndex = (currentIndex + steps) % numbers.length;
-
-            while (participantTracking[currentIndex] == 0) {
-                currentSteps++;
-                currentIndex = (currentIndex + currentSteps) % numbers.length;
-            }
-            participantsCount--;
+        while (circle.size() != 1) {
+            int steps = numbers[circle.get(currentIndex)];
+            circle.remove(currentIndex);
+            currentIndex = (currentIndex + steps - 1) % circle.size();
         }
 
-        System.out.println(Arrays.asList(participantTracking).indexOf(1) + 1);
+        System.out.println(circle.get(0) + 1);
     }
 }
