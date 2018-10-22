8183279a469040af856b8b607967434123df6361
OTPOBEH <3akobah@gmail.com>
2018-10-22 13:00:18 +0300

    Message In a Bottle started...

---
+++ src/MessageInABottle.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class MessageInABottle {
+    public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+
+
+
+
+
+    }
+}
