5a5b06cbeebfdeb6540d105c8ca8877b7580f903
3akobah <3akobah@gmail.com>
2019-01-31 22:54:38 +0200

    Horse Path solved

--- src/HorseMatrix.java
+++ src/HorseMatrix.java
@@ -1,19 +1,30 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class HorseMatrix {
+
+    private static int[] rowMod = new int[]{-2, -2, -1, -1, 1, 1, 2, 2};
+    private static int[] colMod = new int[]{-1, 1, -2, 2, -2, 2, -1, 1};
+
+    private static char[][] field;
+    private static boolean[][] visited;
+
     public static void main(String[] args) throws Exception {
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
         int size = Integer.parseInt(reader.readLine());
 
-        char[][] field = new char[size][size];
-        int[][] stepsCount = new int[size][size];
+        field = new char[size][size];
+        visited = new boolean[size][size];
 
-        int[] startCoords = new int[2];
-        int[] endCoords = new int[2];
+        int startY = 0;
+        int startX = 0;
+
+        int endY = 0;
+        int endX = 0;
 
         for (int i = 0; i < size; i++) {
             String currentRowInput = reader.readLine();
@@ -25,11 +36,11 @@ public class HorseMatrix {
                     continue;
                 } else if (Character.isAlphabetic(currentChar)) {
                     if (currentChar == 's') {
-                        startCoords[0] = i;
-                        startCoords[1] = j;
+                        startY = i;
+                        startX = j;
                     } else if (currentChar == 'e') {
-                        endCoords[0] = i;
-                        endCoords[1] = j;
+                        endY = i;
+                        endX = j;
                     }
                 }
                 field[i][j] = currentChar;
@@ -37,22 +48,60 @@ public class HorseMatrix {
                 j++;
             }
         }
-        System.out.println();
+
+        Node start = new Node(startX, startY);
+        Node end = new Node(endX, endY);
+
+        int distance = bfs(start, end);
+
+        System.out.println(distance == -1? "No" : distance);
     }
 
-    private static int CountShortestPath(int[][]field, int[][] stepsCount, int[] startCoords, int[] endCoords) {
+    private static int bfs(Node start, Node current) {
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(current);
 
-        int[] horseMovementRow = new int[]{-2, -2, -1, -1, 1, 1, 2, 2};
-        int[] horseMovementCol = new int[]{-1, 1, -2, 2, -2, 2, -1, 1};
+        while (!queue.isEmpty()) {
+            current = queue.remove();
+            if (current.col == start.col && current.row == start.row) {
+                return current.distance;
+            }
 
-        Stack<Integer> rowStatus = new Stack<>();
-        Stack<Integer> colStatus = new Stack<>();
+            for (int i = 0; i < colMod.length; i++) {
+                int nextY = current.row + rowMod[i];
+                int nextX = current.col + colMod[i];
 
+                if (isValid(nextX, nextY)) {
+                    Node nextNode = new Node(nextX, nextY);
+                    visited[nextNode.row][nextNode.col] = true;
 
+                    nextNode.distance = current.distance + 1;
+                    queue.add(nextNode);
+                }
+            }
+        }
 
-
-        return 0;
+        return -1;
     }
 
+    private static boolean isValid(int col, int row) {
+        return col >= 0
+                && row >= 0
+                && col < field.length
+                && row < field[0].length
+                && field[row][col] != 'x'
+                && !visited[row][col];
+    }
+
+    static class Node {
+        int col;
+        int row;
+        int distance;
+
+
+        Node(int col, int row) {
+            this.col = col;
+            this.row = row;
+        }
+    }
 }
-//Not Solved!!!
