c2c5b81824d33ddc8ce89890609485e0faec3a31
Stefan Petrov <>
2019-07-10 16:49:15 +0300

    Test

--- src/Test.java
+++ src/Test.java
@@ -2,7 +2,7 @@
 public class Test {
     public static void main(String args[]) {
         FunctionalInterface functionalInterface = System.out::println;
-
+//Comment for test branch
         functionalInterface.writeSomething("some");
     }
 }
